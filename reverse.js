3790ebb51c41f9add1b515625e1f62af09f4b1ad
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-03 02:39:47 +0600

    reverse a string

---
+++ reverse.js
@@ -0,0 +1,30 @@
+
+// reversed string 
+/* 
+const greetings = 'Hello, how are you?'
+
+function reverseString(text){
+    for(const letter of text){
+        console.log(letter);
+    }
+}
+
+const reversed = reverseString(greetings);
+console.log(reversed); */
+
+//
+
+const greetings = 'Hello, how are you?'
+
+function reverseString(text){
+    let reverse = '';
+    for(const letter of text){
+        // console.log(letter);
+        reverse =  letter + reverse;
+    }
+    return reverse;
+}
+
+const reversed = reverseString(greetings);
+console.log(reversed);
+
