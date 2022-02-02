d59db5f8fe85406633f5f51a9e16812a6fd65193
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-03 02:59:37 +0600

    input error file added

---
+++ input-error.js
@@ -0,0 +1,21 @@
+// addition 
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+const total = add(12,3);
+console.log(total);
+
+
+// multiplication
+
+function multiplication(num1, num2){
+    console.log('parameters', num1, num2);
+    const mul = num1 * num2;
+    return mul;
+}
+
+const result = multiplication(2, 4);
+console.log(result);
